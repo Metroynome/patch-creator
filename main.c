de4e4a55d6a8477c9303e2addde639f8dc881afb
Metroynome <yort124@gmail.com>
2023-03-05 15:48:18 -0500

    Updated to create working patch for now

--- main.c
+++ main.c
@@ -1,45 +1,25 @@
 #include <tamtypes.h>
 
-#if PAL
-
-#define MUSIC_LOADED (*(u32*)0x001CF8DC == 0x000FB3B2)
-
-#else
-
-#define MUSIC_LOADED (*(u32*)0x001CF85C == 0x000F8D29)
-
-#endif
+#define DNAS_BYPASS (*(u8*)0x00498CB0)
 
 int main(void)
 {
-	// NTSC: if Music is Loaded
-	if (!MUSIC_LOADED)
-		return -1;
-
 	// This is the Same for NTSC and PAL
 	// Patch.bin hook: 0x00138DD0
 	// run original jal that patch.bin hook took over
 	((void (*)(void))0x001270C0)();
 
 	// DNAS Bypass (For NTSC and PAL)
-	if (*(u8*)0x00498CB0 == (7 || 6))
-		*(u8*)0x00498CB0 = 5;
+	if (DNAS_BYPASS == 7 || DNAS_BYPASS == 6)
+		DNAS_BYPASS = 5;
 
-
-#if PAL
-// PAL
-	// Disable Framelimiter
+	// Disable Framelimiter (PAL)
 	if (*(u32*)0x0021DFE8 == 0x1e)
 		*(u32*)0x0021DFE8 = 0x3c;
 
-#else
-
-// NTSC
-	// Disable Framelimiter
+	// Disable Framelimiter (NTSC)
 	if (*(u32*)0x0021DF60 == 0x1e)
 		*(u32*)0x0021DF60 = 0x3c;
-
-#endif
 
 	return 0;
 }
