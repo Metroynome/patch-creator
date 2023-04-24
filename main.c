1ce3356f266f24b94e1787f15472de2a2862cb15
Metroynome <yort124@gmail.com>
2023-04-23 23:45:53 -0400

    Added Comments and prototype

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #define DNAS_BYPASS (0x00498830)
 #elif !NTSC_J // else if anything but NTSC-J
 #define DNAS_BYPASS (0x00498CB0)
-#else // else (leaves only NTSC=J)
+#else // else (leaves only NTSC-J)
 #define DNAS_BYPASS (0)
 #endif
 
@@ -12,7 +12,7 @@
 #define FRAMELIMITER (*(u32*)0x0021DF60)
 #elif GLADIATOR // Can also use: PAL
 #define FRAMELIMITER (*(u32*)0x0021DFE8)
-#elif PROTOTYPE
+#elif PROTOTYPE // Can also use: PROTO
 #define FRAMELIMITER (*(u32*)0x0021E160)
 #elif GIRIGIRI // Can also use: NTSC_J
 #define FRAMELIMITER (*(u32*)0x00238768)
@@ -26,7 +26,7 @@ int main(void)
 	((void (*)(void))0x001270C0)();
 
 	// DNAS Bypass (For NTSC and PAL)
-	if (DNAS_BYPASS != 0 && (*(u8*)DNAS_BYPASS == 7 || *(u8*)DNAS_BYPASS == 6))
+	if (!DNAS_BYPASS && (*(u8*)DNAS_BYPASS == 7 || *(u8*)DNAS_BYPASS == 6))
 		*(u8*)DNAS_BYPASS = 5;
 
 	// Disable Framelimiter
