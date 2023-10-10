896fabfec824e896cd39c4528823363cd08d9331
Metroynome <yort124@gmail.com>
2023-10-10 15:18:17 -0400

    Added Korean Version

--- main.c
+++ main.c
@@ -2,35 +2,37 @@
 
 #if PROTOTYPE // if Prototype
 #define DNAS_BYPASS (0x00498830)
-#elif !NTSC_J // else if anything but NTSC-J
+#elif NTSC_U || PAL // else if NTSC_U or PAL
 #define DNAS_BYPASS (0x00498CB0)
+#elif KOREA // else if Korean
+#define DNAS_BYPASS (0x004E0B30)
 #else // else (leaves only NTSC-J)
 #define DNAS_BYPASS (0)
 #endif
 
-#if DEADLOCKED // Can also use: NTSC_U
+#if DEADLOCKED // Can also use: NTSC_U, AMERICA, AMERICAN
 #define FRAMELIMITER (*(u32*)0x0021DF60)
-#elif GLADIATOR // Can also use: PAL
+#elif GLADIATOR // Can also use: PAL, EUROPE, EUROPEAN
 #define FRAMELIMITER (*(u32*)0x0021DFE8)
 #elif PROTOTYPE // Can also use: PROTO
 #define FRAMELIMITER (*(u32*)0x0021E160)
-#elif GIRIGIRI // Can also use: NTSC_J
+#elif WRENCHCHAMP // Can also use: NTSC_K, KOREAN, KOREA
+#define FRAMELIMITER (0)
+#elif GIRIGIRI // Can also use: NTSC_J, JAPAN, JAPANESE
 #define FRAMELIMITER (*(u32*)0x00238768)
 #endif
 
 int main(void)
 {
-	// This is the Same for NTSC and PAL
-	// Patch.bin hook: 0x00138DD0
-	// run original jal that patch.bin hook took over
+	// This is the Same for all games
 	((void (*)(void))0x001270C0)();
 
 	// DNAS Bypass (For NTSC and PAL)
-	if (!DNAS_BYPASS && (*(u8*)DNAS_BYPASS == 7 || *(u8*)DNAS_BYPASS == 6))
+	if (DNAS_BYPASS && (*(u8*)DNAS_BYPASS == 7 || *(u8*)DNAS_BYPASS == 6))
 		*(u8*)DNAS_BYPASS = 5;
 
 	// Disable Framelimiter
-	if (FRAMELIMITER == 0x1e)
+	if (FRAMELIMITER && FRAMELIMITER == 0x1e)
 		FRAMELIMITER = 0x3c;
 
 	return 0;
